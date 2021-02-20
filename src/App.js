92d50b888954412b273b291342aed0541e21bb29
Sean Melody <seandmelody@gmail.com>
2021-02-19 20:16:18 -0800

    API call rendering to the screen

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import './App.css';
 // import API from './Components/API/API';
 import Main from './Components/Main/Main';
 import Nav from './Components/Nav/Nav';
+import API from './Utils/API';
+
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Main />
-
+      <API />
     </div>
   );
 }
--- src/Utils/API.js
+++ src/Utils/API.js
@@ -1,5 +1,5 @@
-import axios from "axios";
-const requestUrl = "https://randomuser.me/api/?"
+// import axios from "axios";
+// const requestUrl = "https://randomuser.me/api/?"
 
 
 // export default {
@@ -34,18 +34,64 @@ const requestUrl = "https://randomuser.me/api/?"
 
 //AXIOS
 
-let employeeList = {}
+// let employeeList = {}
 
-axios.get('https://randomuser.me/api/?results=10')
-    .then(function (response) {
-        // handle success
-        // console.log(response);
-        employeeList = response.data.results
-        console.log(employeeList)
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    })
+// axios.get('https://randomuser.me/api/?results=10')
+//     .then(function (response) {
+//         // handle success
+//         // console.log(response);
+//         employeeList = response.data.results
+//         console.log(employeeList)
+//     })
+//     .catch(function (error) {
+//         // handle error
+//         console.log(error);
+//     })
 
-export default {}
+// export default {}
+
+import React from 'react';
+
+class API extends React.Component {
+    state = { people: [], isLoading: true, error: null };
+
+    async componentDidMount() {
+        try {
+            const response = await fetch('https://randomuser.me/api/?results=10');
+            const data = await response.json();
+            this.setState({ people: data.results, isLoading: false });
+
+        } catch (error) {
+            this.setState({ error: error.message, isLoading: false });
+        }
+    }
+
+    renderPerson = () => {
+        const { people, isLoading, error } = this.state;
+
+        if (error) {
+            return <div>{error}</div>;
+        }
+
+        if (isLoading) {
+            return <div>Loading...</div>;
+        }
+
+        return people.map(person => (
+            <div key={person.id.value}>
+                <img src={person.picture.medium} alt="avatar" />
+                <p>First Name: {person.name.first}</p>
+                <p> Last Name: {person.name.last}</p>
+            </div>
+        ));
+    };
+
+    render() {
+        return <div>{this.renderPerson()}</div>;
+    }
+}
+
+export default API;
+
+// THANKS SakoBu
+// https://stackoverflow.com/questions/48892435/making-an-api-call-in-react
