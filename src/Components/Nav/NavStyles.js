77124ca3f24572f299546cca019de7c5c1c53272
Sean Melody <seandmelody@gmail.com>
2021-02-23 14:41:42 -0800

    Navbar styling adjusted

--- src/Components/Nav/NavStyles.js
+++ src/Components/Nav/NavStyles.js
@@ -7,7 +7,6 @@ module.exports = {
         height: "15vh", //ViewHeight
         minHeight: "50px",
         backgroundColor: "black",
-        display: "flex", //Sets items in line
         justifyContent: "space-around" //Puts some spaces between the words in the navbar
     }
 
