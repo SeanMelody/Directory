3970ebe394541c87022e90c9b50a68d03d2cf702
Sean Melody <seandmelody@gmail.com>
2021-02-23 16:43:44 -0800

    Google Font added for header

--- src/Components/Nav/NavStyles.js
+++ src/Components/Nav/NavStyles.js
@@ -3,9 +3,11 @@ module.exports = {
     // Navbar Styling!
     NavBar: {
         paddingTop: "30px",
+        fontSize: "65px",
         color: "black",
         height: "10vh", //ViewHeight
         minHeight: "50px",
+        fontFamily: 'Headland One, Serif',
         // backgroundColor: "black",
         justifyContent: "space-around" //Puts some spaces between the words in the navbar
     }
